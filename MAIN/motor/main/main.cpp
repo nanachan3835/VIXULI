738cfb6479df565a078d8003cdc1352d2e0c0104
NANACHANUWU <duongquocdung11042003@gmail.com>
2024-12-25 21:02:13 +0700

    new game

--- MAIN/motor/main/main.cpp
+++ MAIN/motor/main/main.cpp
@@ -14,7 +14,20 @@
 #include "esp_sleep.h"
 #include "esp_adc_cal.h"
 //#include "driver/pcnt.h"
+#include "lcd2024.h"
+#include "driver/i2c.h"
 
+
+
+//lcd 1602
+#define I2C_MASTER_NUM I2C_NUM_0               // I2C port number for master
+#define I2C_MASTER_SCL_IO 22                   // GPIO number for I2C SCL
+#define I2C_MASTER_SDA_IO 21                   // GPIO number for I2C SDA
+#define I2C_MASTER_FREQ_HZ 100000              // I2C master clock frequency
+#define LCD_ADDR 0x27                          // I2C address of the LCD with PCF8574
+#define WRITE_BIT I2C_MASTER_WRITE             // I2C master write
+#define ACK_CHECK_EN 0x1                       // Enable ACK check
+#define TAG "LCD1602"
 
 
 // Cấu hình GPIO
@@ -180,54 +193,6 @@ int button3_logic()
     }
 }
 
-////////////////////////////////////////////////
-// Hàm xử lý tín hiệu từ rotary encoder (ngắt)
-// static void IRAM_ATTR rotary_isr_handler(void* arg) {
-//     if (!speed_control_enabled) 
-//     {
-//         return;
-//     }
-
-
-//     int clk_state = gpio_get_level(ROTARY_CLK_GPIO);
-//     int dt_state = gpio_get_level(ROTARY_DT_GPIO);
-
-//     if (clk_state != last_clk_state) {
-//         if (dt_state != clk_state) {
-//             motor_speed += 10; // Tăng tốc
-//         } else {
-//             motor_speed -= 10; // Giảm tốc
-//         }
-
-//         // Giới hạn tốc độ trong khoảng [-255, 255]
-//         if (motor_speed > 255) motor_speed = 255;
-//         if (motor_speed < -255) motor_speed = -255;
-
-//         last_clk_state = clk_state;
-//     }
-// }
-
-
-
-// // Hàm khởi tạo Rotary Encoder
-// void rotary_encoder_init() {
-//     // Cấu hình GPIO cho CLK và DT
-//     gpio_config_t io_conf = {
-//         .pin_bit_mask = (1ULL << ROTARY_CLK_GPIO) | (1ULL << ROTARY_DT_GPIO),
-//         .mode = GPIO_MODE_INPUT,
-//         .pull_up_en = GPIO_PULLUP_ENABLE,
-//         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-//         .intr_type = GPIO_INTR_ANYEDGE
-//     };
-//     gpio_config(&io_conf);
-
-//     // Đăng ký ngắt cho CLK
-//     gpio_install_isr_service(0);
-//     gpio_isr_handler_add(ROTARY_CLK_GPIO, rotary_isr_handler, NULL);
-
-//     // Lấy trạng thái ban đầu của CLK
-//     last_clk_state = gpio_get_level(ROTARY_CLK_GPIO);
-// }
 
 
 
@@ -235,15 +200,7 @@ int button3_logic()
 
 
 
-/// dung num xoay de dieu chinh toc do motor
-// static bool example_pcnt_on_reach(pcnt_unit_handle_t unit, const pcnt_watch_event_data_t *edata, void *user_ctx)
-// {
-//     BaseType_t high_task_wakeup;
-//     QueueHandle_t queue = (QueueHandle_t)user_ctx;
-//     // send event data to queue, from this interrupt callback
-//     xQueueSendFromISR(queue, &(edata->watch_point_value), &high_task_wakeup);
-//     return (high_task_wakeup == pdTRUE);
-// }
+
 
 
 
@@ -270,75 +227,15 @@ int convert_voltage_to_speed(uint32_t voltage) {
 //extern "C" void app_main(void)
 void event_handler(void)
 {
-//     //khai bao num xoay
-//    ESP_LOGI(TAG, "install pcnt unit");
-//     pcnt_unit_config_t unit_config = {
-//         .low_limit = EXAMPLE_PCNT_LOW_LIMIT,
-//         .high_limit = EXAMPLE_PCNT_HIGH_LIMIT
-//         //.low_limit = EXAMPLE_PCNT_LOW_LIMIT
-//     };
+/////////////lcd 
+    ESP_LOGI(TAG, "Initializing I2C...");
+    ESP_ERROR_CHECK(i2c_master_init());
 
-//     pcnt_unit_handle_t pcnt_unit = NULL;
-//     ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
-
-//     ESP_LOGI(TAG, "set glitch filter");
-//     pcnt_glitch_filter_config_t filter_config = {
-//         .max_glitch_ns = 1000,
-//     };
-//     ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
-
-//     ESP_LOGI(TAG, "install pcnt channels");
-//     pcnt_chan_config_t chan_a_config = {
-//         .edge_gpio_num = EXAMPLE_EC11_GPIO_A,
-//         .level_gpio_num = EXAMPLE_EC11_GPIO_B,
-//     };
-//     pcnt_channel_handle_t pcnt_chan_a = NULL;
-//     ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_a_config, &pcnt_chan_a));
-//     pcnt_chan_config_t chan_b_config = {
-//         .edge_gpio_num = EXAMPLE_EC11_GPIO_B,
-//         .level_gpio_num = EXAMPLE_EC11_GPIO_A,
-//     };
-//     pcnt_channel_handle_t pcnt_chan_b = NULL;
-//     ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_b_config, &pcnt_chan_b));
-
-//     ESP_LOGI(TAG, "set edge and level actions for pcnt channels");
-//     ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE));
-//     ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
-//     ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
-//     ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
-
-//     ESP_LOGI(TAG, "add watch points and register callbacks");
-//     int watch_points[] = {EXAMPLE_PCNT_LOW_LIMIT, -50, 0, 50, EXAMPLE_PCNT_HIGH_LIMIT};
-//     for (size_t i = 0; i < sizeof(watch_points) / sizeof(watch_points[0]); i++) {
-//         ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, watch_points[i]));
-//     }
-//     pcnt_event_callbacks_t cbs = {
-//         .on_reach = example_pcnt_on_reach,
-//     };
-//     QueueHandle_t queue = xQueueCreate(10, sizeof(int));
-//     ESP_ERROR_CHECK(pcnt_unit_register_event_callbacks(pcnt_unit, &cbs, queue));
-
-//     ESP_LOGI(TAG, "enable pcnt unit");
-//     ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
-//     ESP_LOGI(TAG, "clear pcnt unit");
-//     ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
-//     ESP_LOGI(TAG, "start pcnt unit");
-//     ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
-
-// #if CONFIG_EXAMPLE_WAKE_UP_LIGHT_SLEEP
-//     // EC11 channel output high level in normal state, so we set "low level" to wake up the chip
-//     ESP_ERROR_CHECK(gpio_wakeup_enable(EXAMPLE_EC11_GPIO_A, GPIO_INTR_LOW_LEVEL));
-//     ESP_ERROR_CHECK(esp_sleep_enable_gpio_wakeup());
-//     ESP_ERROR_CHECK(esp_light_sleep_start());
-//     #endif
-
-//     // Report counter value
-//     int pulse_count = 0;
-//     int event_count = 0;
-
-    //configure_adc();
-    //rotary_encoder_init();
-
+    ESP_LOGI(TAG, "Initializing LCD...");
+    ESP_ERROR_CHECK(lcd_init());
+    
+    ESP_LOGI(TAG, "Displaying message...");
+/////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //sudung potentiometer de dieu chinh toc do motor
@@ -356,28 +253,22 @@ void event_handler(void)
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     motor_control_init();
     //StateofMotor Motor_state_1(0,0,0,0,0);
     configure_button();
     /////////////////////////////////////////////////main loop////////////////////
+
+    lcd_print(" NOOOBBBBB");
     while(1)
     {
-
-
-
-
     int raw_value = adc1_get_raw(ADC_CHANNEL);
     uint32_t voltage = esp_adc_cal_raw_to_voltage(raw_value, adc_chars);
     printf("Raw: %d\tVoltage: %" PRIu32 "mV\tSpeed: %d\n", raw_value, voltage, motor_speed);
     
 
 
-        ///khoi tao logic cho nut bam
-    //Motor_state_1.setButton1(button1_logic());
-    //Motor_state_1.setButton2(button2_logic());
-    //Motor_state_1.setButton3(button3_logic());
-    if(gpio_get_level(BUTTON_PIN_1) == 1)
+    if(gpio_get_level(BUTTON_PIN_1) == 0)
         //Motor_state_1.CheckButton1() == 0)
     {
         printf("Button 1 is pressed\n");
@@ -399,24 +290,16 @@ void event_handler(void)
     }
     else
     {
-        printf("Button 1 is pressed\n");
+        printf("Button 1 is not pressed\n");
         speed_control_enabled = 0;
         if (gpio_get_level(BUTTON_PIN_2) == 0)
             //Motor_state_1.CheckButton2() == 1)
         {
-            printf("Button 2 is not pressed\n");
+            printf("Button 2 is press pressed\n");
             speed_control_enabled = 1;
             while(1)
             {
-        //         if (xQueueReceive(queue, &event_count, pdMS_TO_TICKS(1000))) {
-        //                 ESP_LOGI(TAG, "Watch point event, count: %d", event_count);
-        //             } 
-        // else {
-        //     ESP_ERROR_CHECK(pcnt_unit_get_count(pcnt_unit, &pulse_count));
-        //     ESP_LOGI(TAG, "Pulse count: %d", pulse_count);
-        // }
-        //         //rotary_encoder_init();
-        //         motor_speed = pulse_count;
+
                 motor_speed = convert_voltage_to_speed(voltage);
                 motor_set_speed(motor_speed);
                 printf("Raw: %d\tVoltage: %" PRIu32 "mV\tSpeed: %d\n", raw_value, voltage, motor_speed);
@@ -447,4 +330,5 @@ void event_handler(void)
 extern "C" void app_main(void)
 {
     event_handler();
+    
 }
